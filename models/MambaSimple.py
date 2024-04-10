acc563eb887adfedb89a2c59c0cd0aac30106f76
maniaa1822 <manias.1822363@studenti.uniroma1.it>
2024-04-10 17:12:42 +0200

    first committe

--- models/MambaSimple.py
+++ models/MambaSimple.py
@@ -3,7 +3,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from einops import rearrange, repeat, einsum
+#from einops import rearrange, repeat, einsum
 
 from layers.Embed import DataEmbedding
 
