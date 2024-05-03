42832158dcda1331227e7478045b6ed88a6c7008
Gabriele <scognamiglio.1933066@studenti.uniroma1.it>
2024-05-03 17:33:21 +0200

    edit data_factory

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,4 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
+from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
@@ -9,13 +8,7 @@ data_dict = {
     'ETTm1': Dataset_ETT_minute,
     'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
-    'm4': Dataset_M4,
-    'PSM': PSMSegLoader,
-    'MSL': MSLSegLoader,
-    'SMAP': SMAPSegLoader,
-    'SMD': SMDSegLoader,
-    'SWAT': SWATSegLoader,
-    'UEA': UEAloader
+    'm4': Dataset_M4
 }
 
 
